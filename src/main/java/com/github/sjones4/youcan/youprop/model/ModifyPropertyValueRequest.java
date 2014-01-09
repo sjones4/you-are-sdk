92cbbf2eaecb286b11bc4e11e829c1f203e0127a
Steve Jones <steve.jones@eucalyptus.com>
2014-01-08 22:59:12 -0800

    Fix copyright/licence headers

--- src/main/java/com/github/sjones4/youcan/youprop/model/ModifyPropertyValueRequest.java
+++ src/main/java/com/github/sjones4/youcan/youprop/model/ModifyPropertyValueRequest.java
@@ -1,22 +1,17 @@
-/*************************************************************************
- * Copyright 2009-2014 Eucalyptus Systems, Inc.
+/*
+ * Copyright 2014 Steve Jones. All Rights Reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3 of the License.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  http://aws.amazon.com/apache2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses/.
- *
- * Please contact Eucalyptus Systems, Inc., 6755 Hollister Ave., Goleta
- * CA 93117, USA or visit http://www.eucalyptus.com/licenses/ if you need
- * additional information or have any questions.
- ************************************************************************/
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package com.github.sjones4.youcan.youprop.model;
 
 import java.io.Serializable;
